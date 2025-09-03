9db7249b3a29f2aa37df34a521aa37201ad15332
Reetika-Chavan <reetika.chavan@contentstack.com>
2025-09-03 18:08:34 +0530

    isr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
-export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone', // bundles a Node server into dist/
-  }),
-});
+// https://astro.build/config
+export default defineConfig({});
